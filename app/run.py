62b000f83331bf8ca3ca4cb3bee0ccf3d0108998
Theobald <dltheobald@deloitte.co.uk>
2019-10-28 10:37:05 +0000

    Fixed and updated code for app to run
    Imported tokenize into run.py and created a make_graph method to do standard graphs
    Also, added 2 new visuals.
    
    Changed master.html as in original formate, the code wouldn't display images.
    
    Didn't realise that message would be the only input for run.py so had to recode train_classify.py
    and re-run the code to get an updated model for testing. Removed the messages functions and added
    text_length function.

--- app/run.py
+++ app/run.py
@@ -1,73 +1,100 @@
 import json
 import plotly
 import pandas as pd
+import re
 
-from nltk.stem import WordNetLemmatizer
-from nltk.tokenize import word_tokenize
+import nltk
 
 from flask import Flask
 from flask import render_template, request, jsonify
-from plotly.graph_objs import Bar
+from plotly import graph_objs as go
+from plotly.graph_objs import Bar, Figure
 from sklearn.externals import joblib
 from sqlalchemy import create_engine
+
+import sys
+sys.path.append('..')
+from models.train_classifier import text_length, tokenize
 
 
 app = Flask(__name__)
 
-def tokenize(text):
-    tokens = word_tokenize(text)
-    lemmatizer = WordNetLemmatizer()
-
-    clean_tokens = []
-    for tok in tokens:
-        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
-        clean_tokens.append(clean_tok)
-
-    return clean_tokens
-
 # load data
-engine = create_engine('sqlite:///../data/YourDatabaseName.db')
-df = pd.read_sql_table('YourTableName', engine)
+engine = create_engine('sqlite:///../data/DisasterResponse.db')
+df = pd.read_sql_table('cleaned_messages', engine)
 
 # load model
-model = joblib.load("../models/your_model_name.pkl")
+model = joblib.load("../models/classifier.pkl")
 
+def make_graph(data, title, x_label, y_label):
+    """make_graph creates a dictionary/json-format version of a graph which can be used to
+    generate a graph in html.
+
+    Parameters:
+    data (Plotly GraphObject): data containing x and y points in graph object format
+    title (String): The total desired on the graph on the webpage
+    x_label (String): The label desired on the x axis
+    y_label (String): THe label desired on the y axis
+
+    Returns:
+    graph (dict): A dictionary containing the data and labels neccessary to generate a graph
+    """
+    graph = {'data': [data],
+            'layout': {
+                'title': title,
+                'yaxis': {'title': x_label},
+                'xaxis': {'title': y_label}
+            }}
+    
+    return graph
 
 # index webpage displays cool visuals and receives user input text for model
 @app.route('/')
 @app.route('/index')
 def index():
-    
-    # extract data needed for visuals
-    # TODO: Below is an example - modify to extract data for your own visuals
+    graphs = []
+
     genre_counts = df.groupby('genre').count()['message']
     genre_names = list(genre_counts.index)
     
-    # create visuals
-    # TODO: Below is an example - modify to create your own visuals
-    graphs = [
-        {
-            'data': [
-                Bar(
-                    x=genre_names,
-                    y=genre_counts
-                )
-            ],
+    graph_1 = make_graph(Bar(x=genre_names,y=genre_counts),
+                        'Distribution of Message Genres',
+                        'Count', 'Genre')
 
-            'layout': {
-                'title': 'Distribution of Message Genres',
-                'yaxis': {
-                    'title': "Count"
-                },
-                'xaxis': {
-                    'title': "Genre"
-                }
-            }
-        }
-    ]
+    graphs.append(graph_1)
+
+
+    df_melt = pd.melt(df[list(df.columns[3:])], id_vars = ['genre'], var_name = 'categories', value_name = 'Flag' )
     
+    data_test = []
+    for genre in genre_names:
+        categories_sum = df_melt[df_melt['genre'] == genre].groupby('categories')['Flag'].sum()
+        categories = list(categories_sum.index)
+
+        trace = Bar(name = genre, x=categories, y=categories_sum)
+        data_test.append(trace)
+    graph_2 = Figure(data = data_test)
+    graph_2.update_layout(barmode = 'group', title ='Number of tags for each Category and Genre',
+                            xaxis = {'title': 'Categories'}, yaxis = {'title' : 'No. of Tags'})
+
+    graphs.append(graph_2)
+
+
+
+    df['message_length'] = df['message'].apply(lambda x : len(x))
+
+    length_by_genre = df.groupby('genre')['message_length'].mean()
+    genre_names = list(length_by_genre.index)
+
+    graph_3 = make_graph(Bar(x=genre_names,y=length_by_genre),
+                    'Average Message Length by Genre',
+                    'Message Length', 'Genre')             
+    
+    graphs.append(graph_3)
+
     # encode plotly graphs in JSON
     ids = ["graph-{}".format(i) for i, _ in enumerate(graphs)]
+    
     graphJSON = json.dumps(graphs, cls=plotly.utils.PlotlyJSONEncoder)
     
     # render web page with plotly graphs
@@ -93,7 +120,8 @@ def go():
 
 
 def main():
-    app.run(host='0.0.0.0', port=3001, debug=True)
+    #app.run(host='0.0.0.1', port=3001, debug=True)
+    app.run(debug = True)
 
 
 if __name__ == '__main__':
